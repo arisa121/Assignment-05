87e799717c250d115f1a8cc6867f32ba2f057608
Proggramming <engineercse19@gmail.com>
2025-08-29 22:01:55 +0600

    javascript are finally finished

--- js/script.js
+++ js/script.js
@@ -1,26 +1,59 @@
+const HeartButtons = document.querySelectorAll("#heart-icon");
 
-document.getElementById("heart-icon").addEventListener("click", function () {
-
+  HeartButtons.forEach(buttonHeart => {
+    buttonHeart.addEventListener("click", () => {
     const countElement = document.getElementById('heart-count');
     let count = parseInt(countElement.textContent);
     count += 1;
     countElement.textContent = count;
     console.log(countElement.textContent);
-   
+     
+    });
   });
 
+const callButtons = document.querySelectorAll(".call-btn");
+const alartMessage =[
+  'Calling National Emergency Number 999 ',
+  'Calling Police Helpline Number 999 ',
+  'Calling Fire Service Number 999 ',
+  'Calling Ambulance Service Number 1994-999999 ',
+  'Calling Women & Child Helpline 109 ',
+  'Calling Anti-Corruption Helpline Number 106',
+  'Calling Electricity Helpline Number 16216 ',
+  'Calling Brac Helpline Number 16445',
+  'Calling Bangladesh Railway Helpline Number 163'
+]
+const callHistory = document.getElementById('callHistoryList')
 
-  document.getElementById('call-btn').addEventListener('click', function(){
-    
-    const coin = parseInt(document.getElementById('coin-btn').textContent);
-    
+  callButtons.forEach((button,index) => {
+    button.addEventListener("click", () => {
+     const coin = parseInt(document.getElementById('coin-btn').textContent);
+     const callBox = button.closest('.call-box');
+     const title = callBox.querySelector('.call-title').textContent.trim()
+     const number = callBox.querySelector('.call-number').textContent.trim()
+
+  
     if(coin >=20){
       let newCoinValue = coin - 20;
       document.getElementById('coin-btn').innerText = newCoinValue;
-      window.alert('Calling National Emergency Number 999...')
+    
+      window.alert(alartMessage[index]);
+    const li = document.createElement("li");
+    const time = new Date().toLocaleTimeString();
+    li.innerHTML = `<div style="display: flex; justify-content: space-between;">
+      ${title}
+      <span>${time}</span>
+    </div>
+    <div>${number}</div>
+    `;
+    callHistory.append(li)
     }
     else{
       window.alert('আপনার পর্যাপ্ত কয়েন নেই, কল করতে কমপক্ষে ২০ কয়েন লাগবে');
     }
-
+    });
   });
+  const clearButton = document.getElementById('reset-btn');
+  clearButton.addEventListener("click", () => {
+  callHistory.innerHTML = "";
+});
